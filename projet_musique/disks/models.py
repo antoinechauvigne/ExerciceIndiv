38d5b90dcc1fdf443d09799e8ccdf2ecca89bdb2
Antoine Chauvigne <antoine.chauvigne@sciencespo.fr>
2021-05-12 23:22:48 +0200

    V7 Creation des modeles album, track et artist

--- projet_musique/disks/models.py
+++ projet_musique/disks/models.py
@@ -1,4 +1,42 @@
 from django.db import models
 
 # Create your models here.
-#test
+
+
+class Track(models.Model):
+    name = models.CharField(max_length=200)
+    composer = models.CharField(max_length=200)
+    milliseconds = models.IntegerField()
+    bytes = models.IntegerField()
+    unit_price = models.DecimalField(max_digits=4, decimal_places=2)
+    album = models.ForeignKey('Album', on_delete=models.CASCADE)
+
+
+    class Meta:  # qui est optionnelle
+        verbose_name = "track"
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+class Album(models.Model):
+    title = models.CharField(max_length=200)
+    artist = models.ForeignKey('Artist', on_delete=models.CASCADE)
+
+
+    class Meta:  # qui est optionnelle
+        verbose_name = "album"
+        ordering = ['title']
+
+    def __str__(self):
+        return self.title
+
+class Artist(models.Model):
+    name = models.CharField(max_length=200)
+
+    class Meta:  # qui est optionnelle
+        verbose_name = "artist"
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
