cd9629d125ee9eaf6d78cddf7ab0b0ad78ec495e
Antoine Chauvigne <antoine.chauvigne@sciencespo.fr>
2021-05-16 21:55:33 +0200

    V10 Accueil avec seulement noms des titres

--- projet_musique/disks/views.py
+++ projet_musique/disks/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Album
+
 
 # Create your views here.
+def accueil(request):
+    """ Afficher tous les articles de notre blog """
+    albums = Album.objects.all()  # Nous sélectionnons toutes les instances de la classe Album
+    return render(request, 'disks/accueil.html', {'albums': albums})
