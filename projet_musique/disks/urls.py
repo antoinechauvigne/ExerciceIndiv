f707cab800d459d251c461a319f82f318ab75dcc
Antoine Chauvigne <antoine.chauvigne@sciencespo.fr>
2021-05-16 23:58:44 +0200

    V11 Template lire_album et accueil lies

--- projet_musique/disks/urls.py
+++ projet_musique/disks/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('accueil', views.accueil, name='accueil'),
+    path('album/<int:album_id>', views.lire_album, name='lire_album')
 
 ]
--- projet_musique/disks/views.py
+++ projet_musique/disks/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Album
+from .models import Album, Track
 
 
 # Create your views here.
@@ -7,3 +7,9 @@ def accueil(request):
     """ Afficher tous les articles de notre blog """
     albums = Album.objects.all()  # Nous sélectionnons toutes les instances de la classe Album
     return render(request, 'disks/accueil.html', {'albums': albums})
+
+
+def lire_album(request, album_id):
+    """ Afficher le contenu d'un album """
+    tracks = Track.objects.filter(album=album_id)
+    return render(request, 'disks/lire_album.html', locals())
